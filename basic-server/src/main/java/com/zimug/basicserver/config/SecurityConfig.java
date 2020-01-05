933bc4380d851d38944c5e58e29ae7d600919c24
hanxt <hanxiaotongtong@163.com>
2020-01-05 17:35:33 +0800

    2.6.退出登录功能的实现

--- basic-server/src/main/java/com/zimug/basicserver/config/SecurityConfig.java
+++ basic-server/src/main/java/com/zimug/basicserver/config/SecurityConfig.java
@@ -1,9 +1,6 @@
 package com.zimug.basicserver.config;
 
-import com.zimug.basicserver.config.auth.MyAuthenticationFailureHandler;
-import com.zimug.basicserver.config.auth.MyAuthenticationSuccessHandler;
-import com.zimug.basicserver.config.auth.MyExpiredSessionStrategy;
-import com.zimug.basicserver.config.auth.MyUserDetailsService;
+import com.zimug.basicserver.config.auth.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -31,6 +28,9 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     MyAuthenticationFailureHandler myAuthenticationFailureHandler;
 
     @Resource
+    MyLogoutSuccessHandler myLogoutSuccessHandler;
+
+    @Resource
     MyUserDetailsService myUserDetailsService;
 
     @Resource
@@ -39,7 +39,12 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.rememberMe()
+        http.logout()
+                .logoutUrl("/signout")
+                //.logoutSuccessUrl("/login.html")
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessHandler(myLogoutSuccessHandler)
+             .and().rememberMe()
                 .rememberMeParameter("remember-me-new")
                 .rememberMeCookieName("remember-me-cookie")
                 .tokenValiditySeconds(2 * 24 * 60 * 60)
