d035a432a6c6167acb4de811390cd14e109524ee
hanxt <hanxiaotongtong@163.com>
2020-02-14 10:09:01 +0800

    更新MyRBACService，从内存中获取权限，提升效率

--- basic-server/src/main/java/com/zimug/basicserver/config/auth/MyRBACService.java
+++ basic-server/src/main/java/com/zimug/basicserver/config/auth/MyRBACService.java
@@ -1,12 +1,15 @@
 package com.zimug.basicserver.config.auth;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component("rabcService")
@@ -14,8 +17,8 @@ public class MyRBACService {
 
     private AntPathMatcher antPathMatcher = new AntPathMatcher();
 
-    @Resource
-    private MyRBACServiceMapper myRBACServiceMapper;
+    //@Resource
+    //private MyRBACServiceMapper myRBACServiceMapper;
 
     /**
      * 判断某用户是否具有该request资源的访问权限
@@ -25,18 +28,17 @@ public class MyRBACService {
         Object principal = authentication.getPrincipal();
 
         if(principal instanceof UserDetails){
-            String username = ((UserDetails)principal).getUsername();
+            UserDetails userDetails = ((UserDetails)principal);
+            List<GrantedAuthority> authorityList =
+                    AuthorityUtils.commaSeparatedStringToAuthorityList(request.getRequestURI());
+            return userDetails.getAuthorities().contains(authorityList.get(0));
 
+            /*String username = ((UserDetails)principal).getUsername();
             List<String> urls = myRBACServiceMapper.findUrlsByUserName(username);
-
             return urls.stream().anyMatch(
                     url -> antPathMatcher.match(url,request.getRequestURI())
-            );
-
+            );*/
         }
-
         return false;
     }
-
-
 }
